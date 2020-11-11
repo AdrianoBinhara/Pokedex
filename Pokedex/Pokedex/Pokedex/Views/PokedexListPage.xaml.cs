2ac2e69b7f9697b5a311195bbcd3f1c632cbbe6d
Adriano Binhara <adriano@azuris.com.br>
2020-11-11 00:59:20 -0300

    Pagina principal criada

---
+++ Pokedex/Pokedex/Pokedex/Views/PokedexListPage.xaml.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace Pokedex.Views
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class PokedexListPage : ContentPage
+    {
+        public PokedexListPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
