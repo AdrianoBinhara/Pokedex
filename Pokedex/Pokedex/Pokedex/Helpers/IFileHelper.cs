9a739bc47ead84cdc129a5813cabff2c90cf388e
Adriano Binhara <adriano@azuris.com.br>
2020-11-15 07:32:26 -0300

    Commit parcial LiteDB

---
+++ Pokedex/Pokedex/Pokedex/Helpers/IFileHelper.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Pokedex.Helpers
+{
+    public interface IFileHelper
+    {
+        string GetLocalFilePath(string filename);
+    }
+}
